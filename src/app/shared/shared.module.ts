455c45a87522c7f37b8452d802d2c24177b476d0
Ivo Heberle <ivo.heberle@edu.vs.ch>
2024-02-05 15:57:05 +0100

    chang startup tasks and settings

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExampleAComponent } from './example-a/example-a.component';
 import { ExampleBComponent } from './example-b/example-b.component';
-
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbAlertModule
   ],
   declarations: [
     ExampleAComponent,
